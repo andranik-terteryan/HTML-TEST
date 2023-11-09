3cd92f55e4574fe8dd5cf4d2c8fdab2b36bbafca
andranik-terteryan <andranikterteryan447@gmail.com>
2023-11-09 18:49:49 +0400

    Change sccss structure

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const customSelectTrigger = document.querySelector(".order-section__custom-select-trigger");
-  const customSelect = document.querySelector(".order-section__custom-select");
-  const customSelectOptions = document.querySelector(".order-section__custom-select-options");
-  const customSelectText = document.querySelector(".order-section__custom-select-trigger-text");
-  const rangeInput = document.querySelector('.order-section__form-range__input');
+  const customSelectTrigger = document.querySelector(".custom-select__trigger");
+  const customSelect = document.querySelector(".custom-select");
+  const customSelectOptions = document.querySelector(".custom-select__options");
+  const customSelectText = document.querySelector(".custom-select__selected-text");
+
+  const rangeInput = document.querySelector('.form__range');
   const hamburger = document.querySelector('.app-header__hamburger');
-  const navList = document.querySelector('.app-header__nav');
+  const navList = document.querySelector('.menu');
   const header = document.querySelector('.app-header');
-  const form = document.querySelector('.order-section__form')
+
   const SystemTypeInput = document.getElementById('system_type')
   const orderSection = document.querySelector('.order-section');
   const orderSectionTop = orderSection.getBoundingClientRect().top;
-  const fileInput = document.querySelector(".order-section__form-file");
-  const fileNameDisplay = document.querySelector(".order-section__form-file__label--text");
+  const fileInput = document.querySelector(".upload-file__input");
+  const fileNameDisplay = document.querySelector(".upload-file__text");
 
   fileInput.addEventListener("change", function() {
       if (fileInput.files.length > 0) {
@@ -23,13 +24,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   }); 
 
-  const outPutInputRange = document.querySelector('.range-output')
+  const outPutInputRange = document.getElementById('range-output')
   customSelectTrigger.addEventListener("click", function () {
     customSelect.classList.toggle("active");
   });
 
   customSelectOptions.addEventListener("click", function (event) {
-    if (event.target.classList.contains("order-section__custom-select-option")) {
+    if (event.target.classList.contains("custom-select__option")) {
       customSelectText.textContent = event.target.textContent;
       SystemTypeInput.value = event.target.textContent;
       customSelect.classList.remove("active");
